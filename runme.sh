c36d7647be879a349717157b6ce759c6767ea34c
niil87 <challanikhil@gmail.com>
2022-08-01 01:07:12 +0200

    yes added to more lines

--- runme.sh
+++ runme.sh
@@ -29,12 +29,13 @@ if [ $a -eq 0 ]; then
 
     # installing cv2
 
-    sudo apt install build-essential pkg-config libgtk-3-dev \
+    yes | sudo apt install build-essential pkg-config libgtk-3-dev \
     libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
     libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
     gfortran openexr libatlas-base-dev python3-dev python3-numpy \
     libtbb2 libtbb-dev libdc1394-22-dev libopenexr-dev \
     libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
+    AbortCheck
 
     mkdir opencv_build && cd opencv_build
     git clone https://github.com/opencv/opencv.git
